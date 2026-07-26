e082ac4de9ac0f9fab054a52e3f94132943928cc
sna4an <sna4an287@gmail.com>
2026-07-27 00:12:47 +0700

    fix: restore base path for GitHub Actions deploy, remove docs folder

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ import { Settings } from "./pages/Settings";
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename="/qubicpulse">
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Dashboard />} />
